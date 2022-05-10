84011e30b6de333a118f2f89ff6461f46ab26c1d
Aleksey Volkov <avolkov@dtln.ru>
2022-05-11 00:16:21 +0300

    main sections & navigations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
+import Layout from './layouts/Layout';
+import NotFound from './pages/404';
+import About from './pages/About';
+import Contacts from "./pages/Contacts";
+import Main from "./pages/Main";
+import Catalog from "./pages/Catalog";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Router>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/about">
+                            <About/>
+                        </Route>
+                        <Route exact path="/catalog">
+                            <Catalog/>
+                        </Route>
+                        <Route exact path="/contacts">
+                            <Contacts/>
+                        </Route>
+                        <Route exact path="/">
+                            <Main/>
+                        </Route>
+                        <Route exact path="/404">
+                            <NotFound/>
+                        </Route>
+                        <Route path="*">
+                            <Redirect to="/404"/>
+                        </Route>
+                    </Switch>
+                </Layout>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Catalog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Catalog = () => (
+    <div>Каталог</div>
+);
+
+export default Catalog;
